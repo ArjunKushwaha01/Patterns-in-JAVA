3bbe0690d973a50006c03f4832aa3d2ec170081b
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2024-03-05 22:06:08 +0530

    Patterns in JAVA "(*)"

--- src/Inverted_Half_Pyramid.java
+++ src/Inverted_Half_Pyramid.java
@@ -2,7 +2,7 @@ public class Inverted_Half_Pyramid {
     public static void main(String[] args){
         //        n =4
         int n= 4;
-        for(int i = n;i>=1;i--){
+        for(int i = n;i>=1;i--){//only this changed from previous code
             for (int j = 1;j <=i;j++ ){
                 System.out.print("*");
             }

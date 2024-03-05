0967113808d7eb648d5d1083e232dcb0f06abd22
ArjunKushwaha01 <kushwahaarjun920@gmail.com>
2024-03-05 21:57:05 +0530

    Patterns in JAVA "(*)"

--- src/Half_Pyramid.java
+++ src/Half_Pyramid.java
@@ -1,5 +1,12 @@
 public class Half_Pyramid {
     public static void main(String[] args){
-
+//        n =4
+        int n= 4;
+        for(int i = 1;i<=n;i++){
+            for (int j = 1;j <=i;j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
